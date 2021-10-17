89d206360ae2ed8abecd6be8ad64acd3ec86b76a
Mark Skroba <mark.skroba@slu.edu>
2021-10-16 23:27:00 -0500

    now searches for password of length 1 to keysize

--- crack.c
+++ crack.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-// RECURSIVE
-void guessPassword(char alphabet[], char password[], int keysize, int current_i, char* target, char salt[]) {
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+
+void guessPassword(char password[], int keysize, int current_i, char* target, char salt[]) {
 
 	if (current_i == keysize) {
-		printf("%s\n", password);
 		char* encrypted_password = crypt(password, salt);
 		if (strcmp(target, encrypted_password) == 0) {
 			printf("%s\n", password);
@@ -18,27 +18,24 @@ void guessPassword(char alphabet[], char password[], int keysize, int current_i,
 
 	for (int i = 0; i < 26; i++) {
 		password[current_i] = alphabet[i];
-		guessPassword(alphabet, password, keysize, current_i + 1, target, salt);
+		guessPassword(password, keysize, current_i + 1, target, salt);
 	}
 
 }
 
 int main(int argc, char* argv[]) {
 
-	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
 	int keysize = strtol(argv[1], NULL, 10);
 	char* target = argv[2];
-
-	char password[keysize];
-	for (int i = 0; i < keysize; i++) {
-		password[i] = 'a';
-	}
 
 	char salt[3];
 	strncpy(salt, target, 2);
 
-	guessPassword(alphabet, password, keysize, 0, target, salt);
+	for (int i = 1; i <= keysize; i++) {
+		char password[i];
 
+		guessPassword(password, i, 0, target, salt);
+	}
 	return 0;
 
 }
