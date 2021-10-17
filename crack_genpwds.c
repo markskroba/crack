811e9058a91f5fe49e74169a5a595033875953e1
Mark Skroba <mark.skroba@slu.edu>
2021-10-16 21:36:22 -0500

    encrypting and comparing to given target with hardcoded salt

--- crack_genpwds.c
+++ crack_genpwds.c
@@ -4,19 +4,20 @@
 #include <string.h>
 
 // RECURSIVE
-void guessPassword(char alphabet[], char password[], int keysize) {
+void guessPassword(char alphabet[], char password[], int keysize, char* target) {
 
 	if (keysize == 0) {
-		printf("%s\n", password);
-		// printf("\n");
+		char* encrypted_password = crypt(password, "na");
+		if (strcmp(target, encrypted_password) == 0) {
+			printf("%s\n", password);
+			exit(0);
+		}
 		return;
 	}
 
 	for (int i = 0; i < 26; i++) {
-		// printf("Password: '%s', adding '%c' to it", password, alphabet[i]);
-
 		password[keysize - 1] = alphabet[i];
-		guessPassword(alphabet, password, keysize - 1);
+		guessPassword(alphabet, password, keysize - 1, target);
 	}
 
 }
@@ -25,12 +26,13 @@ int main(int argc, char* argv[]) {
 
 	char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 	int keysize = strtol(argv[1], NULL, 10);
+	char* target = argv[2];
 
 	char password[keysize];
 	for (int i = 0; i < keysize; i++) {
 		password[i] = 'a';
 	}
-	guessPassword(alphabet, password, keysize);
+	guessPassword(alphabet, password, keysize, target);
 
 	return 0;
 
