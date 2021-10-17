6822f67655e9e2127b193fb42cd0918c48397120
Mark Skroba <mark.skroba@slu.edu>
2021-10-16 22:17:49 -0500

    reversing order of generating passwords

--- crack.c
+++ crack.c
@@ -4,9 +4,10 @@
 #include <string.h>
 
 // RECURSIVE
-void guessPassword(char alphabet[], char password[], int keysize, char* target, char salt[]) {
+void guessPassword(char alphabet[], char password[], int keysize, int current_i, char* target, char salt[]) {
 
-	if (keysize == 0) {
+	if (current_i == keysize) {
+		printf("%s\n", password);
 		char* encrypted_password = crypt(password, salt);
 		if (strcmp(target, encrypted_password) == 0) {
 			printf("%s\n", password);
@@ -16,8 +17,8 @@ void guessPassword(char alphabet[], char password[], int keysize, char* target, 
 	}
 
 	for (int i = 0; i < 26; i++) {
-		password[keysize - 1] = alphabet[i];
-		guessPassword(alphabet, password, keysize - 1, target, salt);
+		password[current_i] = alphabet[i];
+		guessPassword(alphabet, password, keysize, current_i + 1, target, salt);
 	}
 
 }
@@ -36,7 +37,7 @@ int main(int argc, char* argv[]) {
 	char salt[3];
 	strncpy(salt, target, 2);
 
-	guessPassword(alphabet, password, keysize, target, salt);
+	guessPassword(alphabet, password, keysize, 0, target, salt);
 
 	return 0;
 
